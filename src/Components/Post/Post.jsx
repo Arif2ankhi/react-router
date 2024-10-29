522ca28a0657cdacd28c8245895a4b2ff3e7ca20
Arif2ankhi <arif2ankhi@gmail.com>
2024-10-29 07:59:33 +0000

    Created react router practice project

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+const Post = ({post}) => {
+
+    const {id, title, } = post;
+
+    const postStyle ={
+        border: '2px solid tomato',
+        padding: '5px',
+        borderRadius: '20px'
+
+    }
+
+    return (
+        <div style = {postStyle}>
+            <h4>Post of id: {id}</h4>
+            <p>{title}</p>
+            <Link to={`/post/${id}`}>Post Details</Link>
+            <Link to={`/post/${id}`}><button>Show Details</button></Link>
+        </div>
+    );
+};
+
+export default Post;
